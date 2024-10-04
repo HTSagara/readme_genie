97b97ecb0502e3fab30638cb4a507617c15959a8
gitdevjin <dev.hjshin@gmail.com>
2024-10-03 22:16:24 -0400

    a new feature for reading .toml config file added

---
+++ loadConfig.py
@@ -0,0 +1,12 @@
+import os
+import toml
+
+def load_config():
+    # Load config file from the project's root directory
+    config_path = os.path.expanduser("~/.readme_genie-config.toml")  # Adjust path to project root
+    if os.path.exists(config_path):
+        config = toml.load(config_path)
+    else:
+        config = {}
+
+    return config
